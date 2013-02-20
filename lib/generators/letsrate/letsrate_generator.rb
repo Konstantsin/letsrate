c76b2f8181448d7f1521ec486b0a401d11d9784f
Devin Walters <devinw@gmail.com>
2013-02-20 12:51:22 -0600

    Whitespace removal

--- lib/generators/letsrate/letsrate_generator.rb
+++ lib/generators/letsrate/letsrate_generator.rb
@@ -1,9 +1,9 @@
 require 'rails/generators/migration'
 class LetsrateGenerator < Rails::Generators::NamedBase
   include Rails::Generators::Migration
-  
-  source_root File.expand_path('../templates', __FILE__)      
-                                                       
+
+  source_root File.expand_path('../templates', __FILE__)
+
   desc "copying jquery.raty files to assets directory ..."
   def copying
     copy_file 'jquery.raty.js', 'app/assets/javascripts/jquery.raty.js'
@@ -12,17 +12,17 @@ class LetsrateGenerator < Rails::Generators::NamedBase
     copy_file 'star-half.png', 'app/assets/images/star-half.png'
     copy_file 'letsrate.js', 'app/assets/javascripts/letsrate.js.erb'
     copy_file 'rater_controller.rb', 'app/controllers/rater_controller.rb'
-  end         
-  
+  end
+
   desc "model is creating..."
-  def create_model     
+  def create_model
     model_file = File.join('app/models', "#{file_path}.rb")
     raise "User model (#{model_file}) must exits." unless File.exists?(model_file)
     class_collisions 'Rate'
-    template 'model.rb', File.join('app/models', "rate.rb")    
+    template 'model.rb', File.join('app/models', "rate.rb")
     template 'cache_model.rb', File.join('app/models', "rating_cache.rb")
-  end                                                                           
-  
+  end
+
   def add_rate_path_to_route
     route "match '/rate' => 'rater#create', :as => 'rate'"
   end
@@ -31,19 +31,19 @@ class LetsrateGenerator < Rails::Generators::NamedBase
   def create_cacheable_migration
     migration_template "cache_migration.rb", "db/migrate/create_rating_caches.rb"
   end
-         
+
   desc "migration is creating ..."
   def create_migration
-    migration_template "migration.rb", "db/migrate/create_rates.rb"    
-  end   
-  
-  
-  private 
+    migration_template "migration.rb", "db/migrate/create_rates.rb"
+  end
+
+
+  private
   def self.next_migration_number(dirname)
     if ActiveRecord::Base.timestamped_migrations
       Time.now.utc.strftime("%Y%m%d%H%M%S%L")
     else
       "%.3d" % (current_migration_number(dirname) + 1)
-    end    
+    end
   end
 end
